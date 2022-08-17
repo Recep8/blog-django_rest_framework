fab070b556cb27c67171a308f48a42455d392af9
Receep.8 <recepkarabulut23@gmail.com>
2022-08-17 11:58:08 +0300

    image_field

--- blog/article/models.py
+++ blog/article/models.py
@@ -6,9 +6,9 @@ class Article(models.Model):
     author = models.ForeignKey("auth.User", verbose_name="Yazar", on_delete=models.CASCADE)
     content = models.TextField(verbose_name="İçerik")
     created_date = models.DateTimeField(auto_now_add=True, verbose_name="Oluşturulma Tarihi")
+    upload_file = models.ImageField(upload_to="images/", null=True, blank=True)
 
     def __str__(self):
         return self.title
 
-class Comment(models.Model):
-    pass
+
